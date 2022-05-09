a8d9906ae603ff18da3f39d6cc712872c1e929b9
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2022-05-09 20:08:11 +0300

    Finish implementing the javascript code for the cart buttons

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+
+
 // HEADPHONES PAGE
-var number=0;
+var number=1;
 document.getElementById("num").innerText=number;
+
+$("#decrease").click(function(){
+    number=number-1;
+    document.getElementById("num").innerText=number;
+});
+
+$("#increase").click(function(){
+    number=number+1;
+    document.getElementById("num").innerText=number;
+});
+
+});

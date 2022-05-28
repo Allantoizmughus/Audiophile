a0c1b9379a29216295dd2bcd3d5374281f4f8de7
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2022-05-28 19:40:00 +0300

    Add a dropdown Menu for the media query screens

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    // DROPDOWN MENU
+$("#navIcon").click(function(){
+    $("#dropMenu").toggle()
+});
 
 // HEADPHONES PAGE
 var number=1;
@@ -14,5 +18,7 @@ $("#increase").click(function(){
     number=number+1;
     document.getElementById("num").innerText=number;
 });
+
+
 
 });
